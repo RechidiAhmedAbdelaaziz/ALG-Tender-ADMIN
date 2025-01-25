cb3ec13f581cc27dcca6822ace7d1ba0623e3b48
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2025-01-25 08:48:47 +0100

    Add cloud storage service registration to dependency locator

--- lib/core/di/locator.dart
+++ lib/core/di/locator.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tender_admin/core/router/router.dart';
+import 'package:tender_admin/core/services/cloudstorage/cloud_storage.service.dart';
+import 'package:tender_admin/core/services/cloudstorage/cloudinary/cloudinary.service.dart';
 import 'package:tender_admin/core/services/dio/dio.service.dart';
 
 part 'dependecy_base.dart';
@@ -19,6 +22,13 @@ Future<void> setupLocator() async {
 
   //Router
   locator.registerSingleton(AppRouter());
+
+  //Cloud storage service
+  locator.registerLazySingleton<CloudinaryService>(() =>
+      kIsWeb ? WebCloudinaryService() : MobileCloudinaryService());
+      
+  locator.registerLazySingleton<CloudStorageService>(
+      () => locator<CloudinaryService>());
 
   await DependecyBase._init();
 
